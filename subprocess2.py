d1bffd30f5d98a2f9eb8d58fe1ad671f44d99e2b
Clement <clement.vcharles@yahoo.com>
2024-05-26 07:06:21 -0400

    new output file

--- subprocess2.py
+++ subprocess2.py
@@ -1,16 +1,20 @@
 import subprocess
 import numpy as np
+import random
+
+def generate_random_number():
+    return random.randint(1000, 9000)
 
 # Define the compiled C++ program path
 program_path = './dym-mod-metro'  # Use './program.exe' for Windows
 
 # Beta0 values
-beta_0 = np.linspace(0,5,50)
+beta_0 = np.linspace(0,20,200)
 
 
 # Run the compiled C++ program with parameters
 def execute(parameters):
-    input = "./dym-mod-metro ./groups/myBT 4 4 4 " + str(float(parameters))+ " 0.000000e+00 0.000000e+00 5357"
+    input = "./dym-mod-metro ./groups/myBT 4 4 4 " + str(float(parameters))+ " 0.000000e+00 0.000000e+00 " + str(generate_random_number())
     result = subprocess.run(input, shell=True, capture_output=True, text=True)
     return result.stdout
 count = 0
